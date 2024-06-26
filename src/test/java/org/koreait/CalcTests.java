7532c6618f10b8d7f3707799b9cae65d91e10fd9
meustar <meustar829@gmail.com>
2024-06-26 14:54:17 +0900

    í메서드 2êt26. t25,26,27 같이보셈

--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -164,14 +164,20 @@ public class CalcTests {
     }
 
     @Test
-    @DisplayName("-(10 + 5) == -15")
+    @DisplayName("10 * (10 + 5) == 25")
     void t27() {
+        assertThat(Calc.run("10 * (10 + 5)")).isEqualTo(25);
+    }
+
+    @Test
+    @DisplayName("-(10 + 5) == -15")
+    void t28() {
         assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
     }
 
     @Test
     @DisplayName("-(8 + 2) * -(7 + 3) + 5 == 105")
-    void t28() {
+    void t29() {
         assertThat(Calc.run("-(8 + 2) * -(7 + 3) + 5")).isEqualTo(105);
     }
 }
